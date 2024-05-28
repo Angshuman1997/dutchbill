b50d8a4232d909d9dafa49b95ba94f921d22bbf6
Angshuman Bardhan <bardhanangshu@gmail.com>
2024-05-28 20:05:14 +0530

    restructure

---
+++ api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./router/UserRoute");
+const adminRoutes = require("./router/AdminRoute");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use("/user/api", userRoutes);
+app.use("/admin/api", adminRoutes);
+
+// Test endpoint
+app.get("/test", (req, res) => {
+  res.send("Test endpoint is working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

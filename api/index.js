3afbaef69200249ab3feab62d8c841bc3366518e
Angshuman Bardhan <bardhanangshu@gmail.com>
2024-08-03 12:49:21 +0530

    re-build code

--- api/index.js
+++ api/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const routes = require("../router/Routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use("/api", routes);
 
 // health endpoint
 app.get("/health", (req, res) => {
